454debfebe7e10c402d56fca904a8dc23bbee920
Marty Corbett <corb054@umn.edu>
2018-05-10 13:43:50 -0500

    PEP formatting

--- api/migrations/0001_initial.py
+++ api/migrations/0001_initial.py
@@ -14,7 +14,10 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Animal',
             fields=[
-                ('id', models.AutoField(auto_created=True, primary_key=True, serialize=False, verbose_name='ID')),
+                ('id', models.AutoField(auto_created=True,
+                                        primary_key=True,
+                                        serialize=False,
+                                        verbose_name='ID')),
                 ('commonName', models.CharField(max_length=20)),
                 ('scientificName', models.CharField(max_length=50)),
                 ('family', models.CharField(max_length=20)),
